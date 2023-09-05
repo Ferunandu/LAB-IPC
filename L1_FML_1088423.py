5109f91fad034e14fc1ec45f74deb3b718e9e29f
Ferunandu <144158000+Ferunandu@users.noreply.github.com>
2023-09-05 12:23:35 -0600

    Add files via upload

---
+++ L1_FML_1088423.py
@@ -0,0 +1,26 @@
+
+# Laboratorio de introduccion a programaciòn seccion: 17
+# 5/09/2023
+# Fermando Motta Letona
+# OBJETIVO 
+# ENTRADA: numero1 y numero2
+# PROCESOS "ejecutar cuatro operaciones para n1 y n2"
+# SALIDA: resultados operacioneles
+
+print("mi primer programa")
+numero1= int(input("ingrese algun numero"))
+numero2= int(input("ingrese algun otro numero"))
+
+#divisiòn
+division = numero1 / numero2
+print("la divisiòn es:", division)
+# multiplicaciòn 
+multi = (numero1 * numero2)
+print("La multiplicaciòn es:",multi)
+# resta
+resta = (numero1 - numero2)
+print("La resta da como resultado:",resta)
+# suma
+suma = (numero1 + numero2 )
+print("La suma da como resultado:" ,suma)
+
