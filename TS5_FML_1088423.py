dea2f3a70ae6a366938634612fccbff2aad5606f
Ferunandu <144158000+Ferunandu@users.noreply.github.com>
2023-09-07 09:26:53 -0600

    Add files via upload

---
+++ TS5_FML_1088423.py
@@ -0,0 +1,21 @@
+# Introducción a la programacion (lab) s17
+# 7/09/2023
+# Fernando Motta Letona 1088423
+# Uso y manejo de variables Sting e input
+# Entrada_----}
+# procesos
+# Salida----
+
+print("Mi segundo Programa")
+sNombre=str(input("Ingrese su nombre"))
+sEdad= str(input("ingrese su edad"))
+sCarrera= str(input("Ingrese su carrera"))
+sCarne= int(input("Ingrese su carné"))
+
+print("Mi segundo programa")
+print("NOMBRE:", sNombre)
+print("EDAD:", sEdad)
+print("CARERRA:",sCarrera)
+print("CARNÉ:", sCarne)
+
+print("Soy",sNombre,"tengo",sEdad,"años y estudio la carrera de",sCarrera,"Mi número de carné es:",sCarne)
