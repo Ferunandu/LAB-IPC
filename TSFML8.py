03f24e4ad99c97b8f98eb2d294f4cb86004d4cc0
Ferunandu <144158000+Ferunandu@users.noreply.github.com>
2023-09-28 09:43:19 -0600

    Add files via upload

---
+++ TSFML8.py
@@ -0,0 +1,42 @@
+# Laboratorio de introduccion a programación seccion: 17
+# 26/09/2023
+# Fermando Motta Letona
+# OBJETIVO: 
+# ENTRADA: 
+# PROCESOS ""
+# SALIDA: 
+
+import math
+
+
+
+for i in range(1, 26,):
+    print(i, end=", ")
+print()
+
+i=1
+while i <= 25:
+    print(i,end=", ")
+    i +=1
+print()
+
+for i in range(5, 51,5):
+    print(i, end=", ")
+print()
+
+i=5
+while i <= 50:
+    print(i,end=", ")
+    i +=5
+print()
+
+for i in range(20, -2,-2):
+    print(i,end=", ")
+print()
+
+i=20
+while i >= 0:
+    print(i,end=", ")
+    i -=2
+print()
+
